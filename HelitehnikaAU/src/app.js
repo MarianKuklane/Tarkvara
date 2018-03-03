fbb5f993e0ada6be4e5b4f896664c1c1c7e4189e
MarianKuklane <marian.kuklane@gmail.com>
2018-03-03 13:17:00 +0200

    Terve projekt

--- HelitehnikaAU/src/app.js
+++ HelitehnikaAU/src/app.js
@@ -1,5 +1,22 @@
-export class App {
-  constructor() {
-    this.message = 'Hello World!';
+export class App
+{
+  constructor() 
+  {
+	    this.message = 'Katsetan veel kord!';
   }
+  
+  configureRouter(config, router) 
+  {
+      this.router = router; 
+      config.title = 'Helitehnika'; 
+      
+      config.map([ 
+    	   { route: ['', 'index'], name: 'pealeht', moduleId: 'vaated/index', nav: true, title:"Pealeht"},
+    	   { route: 'regaette', name: 'ettevõtte_registreerimine', moduleId: 'vaated/ettevõtte_registreerimine', nav: true, title: 'Registeeru'},
+    	   { route: 'regakas', name: 'kasutajaks_registreerimine', moduleId: 'vaated/kasutajaks_registreerimine', nav: true},
+           { route: 'lisa', name: 'lisauus', moduleId: 'vaated/lisauus', nav: true, title: 'Lisa'},
+           { route: 'kuulutusedet', name: 'kuulutuse_detailvaade', moduleId: 'vaated/kuulutuse_detailvaade', title: 'Kuulutuse detailvaade', nav: true}
+        ]);
+  } 
+  
 }
