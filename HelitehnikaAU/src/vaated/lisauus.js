36459837b650a619717016019fc11ee8dea2e6e4
dianneaa <dianne.aalde@gmail.com>
2018-03-03 21:12:33 +0200

    Täiendasin lisauus.html ja lisauus.js faile data bindinguga.

--- HelitehnikaAU/src/vaated/lisauus.js
+++ HelitehnikaAU/src/vaated/lisauus.js
@@ -1,7 +1,31 @@
+import {HttpClient, json} from 'aurelia-fetch-client'
+
 export class index 
 {
+	advertData={}
+	
   constructor()
   {
 
+  }
+  
+  addAdvert(){
+	  let client = new HttpClient();
+	  
+	  let advertData = {
+			  
+	  }
+	  
+	  client.fetch('http://localhost:9000/advert/add', {
+		  'method': "POST",
+		  'body': json(this.advertData)
+	  })
+	      .then(response => response.json())
+	      .then(data => {
+	      console.log(data.description);
+      });
+  
+	  
+	  
   }
 }
