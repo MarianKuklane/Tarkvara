3b80773c23a4bcec55c251d870533a249537c0d5
dianneaa <dianne.aalde@gmail.com>
2018-04-03 12:33:08 +0300

    Pultide lehele kuvatakse andmebaasis kõik kuulutused.

--- HelitehnikaAU/src/vaated/kuulutuste_lisamine.js
+++ HelitehnikaAU/src/vaated/kuulutuste_lisamine.js
@@ -1,8 +1,20 @@
-export class kuulutuse_detailvaade
+import {HttpClient, json} from 'aurelia-fetch-client'
+
+export class kuulutuse_lisamine
 
 {
-  constructor()
-  {
+	advertList = []
 
-  }
+	constructor() {
+		
+	}
+
+	activate() {
+		let client = new HttpClient();
+
+		client.fetch('http://localhost:8080/adverts')
+			.then(response => response.json())
+			.then(adverts => this.advertList = adverts);
+		console.log("ACTIVATE");
+	}
 }
