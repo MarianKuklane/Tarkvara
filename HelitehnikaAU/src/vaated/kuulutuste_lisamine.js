8d097b1e71316ed0d7e952040c30c3c017bdd6fa
dianneaa <dianne.aalde@gmail.com>
2018-04-04 19:05:08 +0300

    Üritan meetodile getAdvertById kuulutuse id sisendparameetrisse anda.

--- HelitehnikaAU/src/vaated/kuulutuste_lisamine.js
+++ HelitehnikaAU/src/vaated/kuulutuste_lisamine.js
@@ -4,6 +4,7 @@ export class kuulutuse_lisamine
 
 {
 	advertList = []
+	advertId = {}
 
 	constructor() {
 		
@@ -17,4 +18,20 @@ export class kuulutuse_lisamine
 			.then(adverts => this.advertList = adverts);
 		console.log("ACTIVATE");
 	}
+	
+	getAdvertById(){
+		  let client = new HttpClient();		  
+		  	  
+		  client.fetch('http://localhost:8080/advert/', {
+			  'method': "GET",
+			  'body': json(this.advertData)
+		  })
+		      .then(response => response.json())
+		      .then(data => {
+		      console.log("Server saatis ");
+	      });
+	  
+		  console.log("Method getAdvertById executed");
+		  
+	  }
 }
