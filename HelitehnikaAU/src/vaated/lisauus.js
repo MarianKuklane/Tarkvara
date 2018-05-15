6d395d6c8299a6ef5cfefa6c48fe56b03818bd19
MarianKuklane <marian.kuklane@gmail.com>
2018-05-15 20:33:52 +0300

    Kujundus + veateade

--- HelitehnikaAU/src/vaated/lisauus.js
+++ HelitehnikaAU/src/vaated/lisauus.js
@@ -4,54 +4,45 @@ import {Router} from 'aurelia-router';
 
 export class lisauus 
 {
-	advertData = {};
-	
-	static inject() { return [Router]; }
-	
-	constructor(router) {
-		this.router = router;
-	}
- 
-  addAdvert(){
-	  let client = new HttpClient();		  
-	  	  
-	  client.fetch('http://localhost:8080/advert/add', {
-		  'method': "POST",
-		  'body': json(this.advertData)
-	  })
-	      .then(response => response.json())
-	      .then(data => {
-	      console.log("Server saatis " + data.title);
-      });
-  
-	  console.log("Method executed");
-	  
-	
-	  var title = document.getElementById("title");
-	  var email = document.getElementById("email");
-	  var phone = document.getElementById("phone");
-	  var price = document.getElementById("price");
-	  var description = document.getElementById("description");
-	  
-	  if (title.value == ""  && email.value == ""  &&  phone.value == ""  &&  price.value ==""  &&  description.value =="")
-		  {
-		  	alert("Sisesta väljad!");
-		  }
-	  
-	  if (title.empty  && email.empty  &&  phone.empty  &&  price.empty ==""  &&  description.empty =="")
+	  advertData = {};
+		
+		static inject() { return [Router]; }
+		
+		constructor(router) {
+			this.router = router;
+		}
+	 
+	  addAdvert()
 	  {
-	  	alert("Sisesta väljad!");
-	  }
+		  let client = new HttpClient();
+	
+		  var title = document.getElementById("title");
+		  var email = document.getElementById("email");
+		  var phone = document.getElementById("phone");
+		  var price = document.getElementById("price");
+		  var description = document.getElementById("description");
+		  var category = document.getElementById("category");
+		  var subcategory = document.getElementById("subcategory");
 
-	if(title.value && email.value && phone.value && price.value && description.value)
-		{
-			document.location.href = "index"
-		} 
-/*	else 
-	{
-		alert("Sisesta kõik väljad!");
-	}*/
-	
-	
-  }
+	  
+		  if (title.value == ""  || email.value == ""  ||  phone.value == ""  ||  price.value ==""  ||  description.value =="" ||  category.value =="Vali" ||  subcategory.value =="Vali")
+		  {
+			  alert("Sisesta väljad!");
+		  }
+		  else
+		  {		  		  	  
+			  client.fetch('http://localhost:8080/advert/add', {
+				  			'method': "POST",
+				  			'body': json(this.advertData)
+			  })
+		      .then(response => response.json())
+		      .then(data => {
+		      console.log("Server saatis " + data.title);
+		      	});
+	  
+		  		console.log("Method executed");
+		  
+		  		document.location.href = "index"
+		  }
+	  }
 }
